1107a61aaf9a843e4f2ed96563959e9728acbeac
Gideon Edoghotu <edoghotugiddy@gmail.com>
2024-10-25 15:35:41 +0100

    updated

---
+++ javascript_intro/session_2/index.js
@@ -0,0 +1,155 @@
+/**
+ * Methods:
+ * toString()
+ * toFixed(); converts a num  to a string with a number of fixed decimal places
+ * 24.7673 ==> 24.7 ; num.toFixed(1)
+ * parseInt; parses a string to return an interger.
+ * parseFloat; for decimal
+ * Math.round; rounds to the nearest whole interger; 2479.774 ==> 2479
+ * 
+ * Conditions;
+ * It allows for decision making in codes. True or False
+ * We have the if, else, elseif statements
+ * if- condition and code to execute. if(..){};
+ * else- if(){} else {};
+ * 
+ * The switch statement; evaluates an expression and executes code that matches a specific case value
+ * if no case value matches, the defaualt value is executed.
+ * The Syntax:
+ * switch(expression){
+ *      case 1:
+ *      ......
+ *      break;
+ * 
+ *      case 2:
+ *      ......
+ *      break;
+ * 
+ *      default;
+ * 
+ * }
+ * 
+ * Tenary Operator is a short hand for the if else statement.
+ * 
+ * Logical Operator (AND&&), (OR||), (NOT!)
+ */
+
+
+/**
+ * A student wrote a test and scored 11/15
+ * He wrote test 2 and scored 4/15
+ * He wrote exam and scored 37/70
+ * 
+ * Grading System:
+ * 0 - 39 - F
+ * 40 - 60 - C
+ * 61 - 69 - B
+ * 70 - 100 - A
+ * 
+ * Arithmetic expressions
+ * Conditions
+ * Logical expressions
+ * 
+ * Sum t1 t2
+ * Sum T Exam
+ * Sum t1 t2 Exam
+ * 
+ * Before you grade user, if the user scores less than 70, add 10.
+ * 
+ * From the final sum, grade user
+ * 
+ * Must use switch statement.
+ */
+
+
+
+
+
+
+
+// let weekend = true;
+// let holiday = false;
+
+// let age = 11;
+// let gender = 'male';
+
+// if(age >11 && gender == 'male'){
+//     console.log('admit')
+// } else {
+//     console.log('not admit')
+// }
+
+// if(weekend || holiday){
+//     console.log('free');
+// }
+// else{
+//     consolee.log('not free');
+// }
+
+
+// let age = 20
+// let canVote = age >= 20 ? "can vote" : "cannot vote"
+
+// console.log(canVote);
+
+// let num = 123;
+// let res = num.toString() + 5
+// let num2 = "24ef"
+
+// let data1 = "247";
+// let data2 = "247";
+// let data3 = 247
+
+// if (data1 == data2){
+//     console.log('they are equal')
+// }
+// else {
+//     console.log('Not equal')
+// }
+
+// if (data1 === data3){
+//     console.log('they have the same data type')
+// }
+// else {
+//     console.log('Not the same data type')
+// }
+
+let day = "friday";
+
+// switch(day){
+//     case "monday":
+//         console.log('today is monday');
+//         break;
+//     case "tuesday":
+//         console.log('today is monday');
+//         break;
+//     case "wednesday":
+//         console.log('today is monday');
+//         break;
+//     case "thursday":
+//         console.log('today is monday');
+//         break;
+//     case "friday":
+//         console.log('today is monday');
+//         break;
+//     case "saturday":
+//         console.log('today is monday');
+//         break;
+//     case "sunday":
+//         console.log('today is monday');
+//         break;
+// }
+
+
+// console.log(num2.parseInt())
+// console.log(parseInt(num2))
+
+// console.log(res)
+
+
+
+// console.log(name2.toUpperCase());
+// console.log(nameTrim.trim());
+// console.log(msg.includes("are"));
+// console.log(text.split(','));
+// console.log(subRes);
